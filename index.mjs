c5597fe175232caa0452b4f5a49b79756cc13b99
coltonh12 <145588758+coltonh12@users.noreply.github.com>
2024-03-15 13:59:42 +0000

    t

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 
 const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
