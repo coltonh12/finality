000b82ace020f6abd23ca89360c13a7f43c97475
coltonh12 <145588758+coltonh12@users.noreply.github.com>
2024-03-15 13:59:26 +0000

    k

--- index.mjs
+++ index.mjs
@@ -1,12 +1,11 @@
-
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
 
 const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getBooks() {
+
 let { data: books1, error } = await supabase
   .from('books1')
   .select('*')
@@ -15,5 +14,5 @@ for (let book of books1) {
   let bookList = document.getElementById('books1');
   bookList.innerHTML += `<li>${book.title}</li>`;
 }
-}
-getBooks();
+
+
